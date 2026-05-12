08fe2de0714aea86c7eff869ec18cb954beffdc4
mahim-x446 <howladermahim5858@gmail.com>
2026-05-13 00:13:57 +0600

    Staff class added

--- Hospital.java
+++ Hospital.java
@@ -1,6 +1,9 @@
 public class Hospital {
     private String hospitalName = "SouthEast Medical Hospital";
     private String hospitalBranch;
+    private Doctor [] doctors;
+    private Patient [] patients;
+    private Staff [] staff;
 
     public Hospital(String branch) {
         this.hospitalBranch = branch;
