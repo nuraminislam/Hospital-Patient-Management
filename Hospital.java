390ff5cdddfb38585917e5239625317295d5c888
mahim-x446 <howladermahim5858@gmail.com>
2026-05-12 20:04:21 +0600

    Hospital class added

---
+++ Hospital.java
@@ -0,0 +1,9 @@
+public class Hospital {
+    public String name;
+    public String branch;
+
+    public Hospital(String name, String branch) {
+        this.name = name;
+        this.branch = branch;
+    }
+}
